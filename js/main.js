59889f924e37532381ee630964af86fb2a0a5418
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-11-13 19:17:10 +1100

    Replace the text

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// ----------------- Button on click -----------------
+// ----------------- 5. Button on click -----------------
 // $("#firstBtn").on("click", function() {
 //   $("p#statement").hide();
 // });
 
 
-// ------------- Event Handeling & Effects -------------
+// ------------- 5. Event Handeling & Effects -------------
 
 // $("#box").show();
 
@@ -15,13 +15,34 @@ $("#target").on("click", function() {
 
   // $("#box").slideDown(1000); // 1 second
   // $("#box").slideUp(2000); // element needs to be showns 1st
-  // $("#box").slideToggle(1000);
+  $("#box").slideToggle(1000);
 
   // $("#box").fadeIn(1000);
   // $("#box").fadeOut(1000);
   // $("#box").fadeTo(1000, 0.5);
-  $("#box").fadeToggle(1000);
+  // $("#box").fadeToggle(1000);
 });
+
+
+
+// ------------- 6. Text, Val & HTML Functions -------------
+$("#btn_main").on("click", function() {
+  $(".dope").text("This is a new sentence.");
+  $("#btn_main").val("Double Click Me");
+});
+
+$("#btn_main").on("dblclick", function() {
+  $(".dope").text("You'll learn how to replace me");
+  $("#btn_main").val("Replace");
+});
+
+
+
+
+
+
+
+
 
 
 
