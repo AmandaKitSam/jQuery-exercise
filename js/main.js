65dbf8d3728aa2c2db27a936a4ce53e5d783cd86
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-11-14 00:56:37 +1100

    appendTo to the right place

--- js/main.js
+++ js/main.js
@@ -1,84 +1,88 @@
-// ----------------- 5. Button on click -----------------
-$("#firstBtn").on("click", function() {
-  $("h4#statement").slideToggle(1000);
-});
+$(document).ready(function() {
 
-$("form").on("submit", function(){
-  var enterChat = $("input#entered_chat").val();
-  alert(enterChat);
-});
+  // ----------------- 5. Button on click -----------------
+  $("#firstBtn").on("click", function() {
+    $("h4#statement").slideDown(1000);
+  });
 
-$("button#send_chat").on("click", function(){
-  var enterChat = $("input#entered_chat").val();
-  $("h4#user-statement").val(enterChat);
-});
+  // $("form").on("submit", function(){
+  //   var enterChat = $("input#entered_chat").val();
+  //   alert(enterChat);
+  // });
+
+  $("button#send_chat").on("click", function(){
+    var enterChat = $("input#entered_chat").val();
+    // console.log(enterChat);
+    // $("h4#user-statement").val(enterChat);
+    $('<h4>Nicholas: "' + enterChat + '"</h4>').appendTo("h4#user-statement");
+  });
 
 
-// ------------- 5. Event Handeling & Effects -------------
+  // ------------- 5. Event Handeling & Effects -------------
 
-// $("#box").show();
-
-$("#target").on("click", function() {
-  // $("p#statement").hide();
   // $("#box").show();
-  // $("#box").toggle();
 
-  // $("#box").slideDown(1000); // 1 second
-  // $("#box").slideUp(2000); // element needs to be showns 1st
-  $("#box").slideToggle(1000);
-
-  // $("#box").fadeIn(1000);
-  // $("#box").fadeOut(1000);
-  // $("#box").fadeTo(1000, 0.5);
-  // $("#box").fadeToggle(1000);
-});
-
-
-
-// ------------- 6. Text, Val & HTML Functions -------------
-$("#btn_main").on("click", function() {
-  $(".dope").text("This is a new sentence.");
-  $("#btn_main").val("Double Click Me");
-});
-
-$("#btn_main").on("dblclick", function() {
-  $(".dope").text("You'll learn how to replace me");
-  $("#btn_main").val("Replace");
-});
-
-
-$("#btn_new").on("click", function() {
-  $(".dope").html('<b class="cool"> Hey there this is HTML</b>');
-});
-
-
-
-// ------------- 7. Form Events & Variables -------------
-$("form").on("submit", function(){
-  var enterWord = $("input#entered_word").val(); // .val() Read the words in the input
-  alert(enterWord);
-});
-
-$("button#loc_button").on("dblclick click", function(){
-  var enteredZip = $("input#entered_zip").val();
-  $("input#entered_city_state").val(enteredZip + " is for NJ");
-});
-
-$("input#entered_zip").on("focus", function(){
-  $("h2#city_display").text("Please type your zipcode");
-});
-
-
-
-
-
-
+  // $("#target").on("click", function() {
+  //   // $("p#statement").hide();
+  //   // $("#box").show();
+  //   // $("#box").toggle();
+  //
+  //   // $("#box").slideDown(1000); // 1 second
+  //   // $("#box").slideUp(2000); // element needs to be showns 1st
+  //   $("#box").slideToggle(1000);
+  //
+  //   // $("#box").fadeIn(1000);
+  //   // $("#box").fadeOut(1000);
+  //   // $("#box").fadeTo(1000, 0.5);
+  //   // $("#box").fadeToggle(1000);
+  // });
+  //
+  //
+  //
+  // // ------------- 6. Text, Val & HTML Functions -------------
+  // $("#btn_main").on("click", function() {
+  //   $(".dope").text("This is a new sentence.");
+  //   $("#btn_main").val("Double Click Me");
+  // });
+  //
+  // $("#btn_main").on("dblclick", function() {
+  //   $(".dope").text("You'll learn how to replace me");
+  //   $("#btn_main").val("Replace");
+  // });
+  //
+  //
+  // $("#btn_new").on("click", function() {
+  //   $(".dope").html('<b class="cool"> Hey there this is HTML</b>');
+  // });
+  //
+  //
+  //
+  // // ------------- 7. Form Events & Variables -------------
+  // $("form").on("submit", function(){
+  //   var enterWord = $("input#entered_word").val(); // .val() Read the words in the input
+  //   // alert(enterWord);
+  // });
+  //
+  // $("button#loc_button").on("dblclick click", function(){
+  //   var enteredZip = $("input#entered_zip").val();
+  //   $("input#entered_city_state").val(enteredZip + " is for NJ");
+  // });
+  //
+  // $("input#entered_zip").on("focus", function(){
+  //   $("h2#city_display").text("Please type your zipcode");
+  // });
 
 
 
 
 
 
-// $(document).ready(function() {
 
-// }); // END.ready function
+
+
+
+
+
+
+
+}); // END.ready function
