30f144f2068e1d8243d7cd9c8a7b564ed5a3f2c5
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-11-13 19:02:06 +1100

    Event Handeling & Effects

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-$("#firstBtn").on("click", function() {
-  $("p#statement").hide();
+// ----------------- Button on click -----------------
+// $("#firstBtn").on("click", function() {
+//   $("p#statement").hide();
+// });
+
+
+// ------------- Event Handeling & Effects -------------
+
+// $("#box").show();
+
+$("#target").on("click", function() {
+  // $("p#statement").hide();
+  // $("#box").show();
+  // $("#box").toggle();
+
+  // $("#box").slideDown(1000); // 1 second
+  // $("#box").slideUp(2000); // element needs to be showns 1st
+  // $("#box").slideToggle(1000);
+
+  // $("#box").fadeIn(1000);
+  // $("#box").fadeOut(1000);
+  // $("#box").fadeTo(1000, 0.5);
+  $("#box").fadeToggle(1000);
 });
+
+
+
+
+
 
 // $(document).ready(function() {
 
